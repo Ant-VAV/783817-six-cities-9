6d44600a9cf51c832c27a08896be9d8a42d0797e
Anton Vdovichenko <ant-vav@skbkontur.ru>
2022-02-18 10:28:31 +0300

    Routes

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,8 +1,27 @@
-import Main, { MainProps } from '../main/main';
+import { Route, Routes } from 'react-router-dom';
+import Main, { MainProps } from '../../pages/main/main';
+import { AuthorizationStatus, Page } from '../../const';
+import { NotFound } from '../../pages/not-found/not-found';
+import { PlacePage } from '../../pages/place-page/place-page';
+import { Favorites } from '../../pages/favorites/favorites';
+import { Login } from '../../pages/login/login';
+import { PrivateRoute } from '../private-route/private-route';
 
 function App(props: MainProps): JSX.Element {
   const { rentOffersTotal } = props;
-  return <Main rentOffersTotal={rentOffersTotal}/>;
+  return (
+    <Routes>
+      <Route path={Page.Main} element={<Main rentOffersTotal={rentOffersTotal}/>}/>
+      <Route path={Page.Place} element={<PlacePage isLoggedIn/>}/>
+      <Route path={Page.Favorites} element={(
+        <PrivateRoute authorizationStatus={AuthorizationStatus.Authorized}>
+          <Favorites/>
+        </PrivateRoute>
+      )}
+      />
+      <Route path={Page.LogIn} element={<Login/>}/>
+      <Route path={'*'} element={<NotFound/>}/>
+    </Routes>);
 }
 
 export default App;
--- project/src/pages/favorites/favorites-empty.tsx
+++ project/src/pages/favorites/favorites-empty.tsx
@@ -1,5 +1,5 @@
-import { Header } from '../layout/header/header';
-import { Footer } from '../layout/footer/footer';
+import { Header } from '../../components/layout/header/header';
+import { Footer } from '../../components/layout/footer/footer';
 
 export function FavoritesEmpty() {
   return (
