e3cd140d263c1931768c818beb45a00e045791c9
Anton Vdovichenko <ant-vav@skbkontur.ru>
2022-04-03 23:33:53 +0300

    Remove mocks

--- project/src/mocks/place-info.ts
+++
@@ -1,5 +0,0 @@
-import { PlaceInfo } from '../types/client';
-import { getPlaceListInfo } from './place-list-info';
-
-export const getPlaceInfo = (id: string): PlaceInfo | undefined =>
-  getPlaceListInfo().find((place) => place.id === parseInt(id, 10));
