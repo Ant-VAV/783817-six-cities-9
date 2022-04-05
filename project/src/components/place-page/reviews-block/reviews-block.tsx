8dc042c65f8749194b320884daaca2d1bbfb0a33
Anton Vdovichenko <ant-vav@skbkontur.ru>
2022-04-05 14:38:52 +0300

    Make review as necessary

--- project/src/components/place-page/reviews-block/reviews-block.tsx
+++ project/src/components/place-page/reviews-block/reviews-block.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect } from 'react';
-import { ReviewItem } from '../review-item/review-item';
 import { ReviewForm } from '../review-form/review-form';
 import { useAppDispatch, useAppSelector } from '../../../hooks/state';
 import { AuthorizationStatus } from '../../../const';
 import { fetchPlaceReviewAction } from '../../../store/api-actions';
 import { getAuthorizationStatus, getReviews } from '../../../store/selectors/selectors';
+import { ReviewItem } from '../review-item/review-item';
+import { dateSortFunction } from '../../../date-helpers';
 
 interface ReviewsProps {
   placeId: number;
@@ -21,14 +22,16 @@ export function ReviewsBlock(props: ReviewsProps) {
     dispatch(fetchPlaceReviewAction(placeId.toString()));
   }, [placeId, dispatch]);
 
-
   return (
     <section className='property__reviews reviews'>
       <h2 className='reviews__title'>Reviews &middot; <span className='reviews__amount'>{reviews?.length || 0}</span>
       </h2>
       {reviews && (
         <ul className='reviews__list'>
-          {reviews.map((review) => <ReviewItem review={review} key={review.id}/>)}
+          {[...reviews]
+            .sort((a, b) => dateSortFunction(a.date, b.date))
+            .slice(0, 10)
+            .map((review) => <ReviewItem review={review} key={review.id}/>)}
         </ul>
       )}
       {isLoggedIn && (
